399966d4513b19dec0bff8858e0a7323cd0f5183
Jordan Hudgens <jordan@devcamp.com>
2017-05-11 22:30:05 +0000

    add edit and update functionality to portfolio's controller

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -8,6 +8,10 @@ class PortfoliosController < ApplicationController
     @portfolio_item = Portfolio.new
   end
   
+  def edit
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+  
   def create
     @portfolio_item = Portfolio.new(portfolio_params)
     
@@ -19,6 +23,18 @@ class PortfoliosController < ApplicationController
       end
     end
     
+  end
+  
+  def update
+    @portfolio_item = Portfolio.find(params[:id])
+    
+    respond_to do |format|
+      if @portfolio_item.update(portfolio_params)
+        format.html{ redirect_to portfolios_path, notice: "Portfolio was successfully updated." }
+      else
+        format.html { render :edit }
+      end
+    end
   end
   
   
