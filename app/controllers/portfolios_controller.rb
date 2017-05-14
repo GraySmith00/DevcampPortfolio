4ff8a9d9017d9ae6b7eb628258488978cebf1aa2
Jordan Hudgens <jordan@devcamp.com>
2017-05-14 03:42:14 +0000

    implemented destroy action for portfolio feature

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,11 +1,14 @@
 class PortfoliosController < ApplicationController
+  before_action :set_portfolio_item, only: [:show, :edit, :update, :destroy]
+  
   
   def index
     @portfolio_items = Portfolio.all
   end
   
   def show
-    @portfolio_item = Portfolio.find(params[:id])
+    #@portfolio_item = Portfolio.find(params[:id])
+    
   end
   
   def new
@@ -13,7 +16,7 @@ class PortfoliosController < ApplicationController
   end
   
   def edit
-    @portfolio_item = Portfolio.find(params[:id])
+    #@portfolio_item = Portfolio.find(params[:id])
   end
   
   def create
@@ -30,14 +33,23 @@ class PortfoliosController < ApplicationController
   end
   
   def update
-    @portfolio_item = Portfolio.find(params[:id])
+    #@portfolio_item = Portfolio.find(params[:id])
     
     respond_to do |format|
       if @portfolio_item.update(portfolio_params)
-        format.html{ redirect_to portfolios_path, notice: "Portfolio was successfully updated." }
+        format.html{ redirect_to portfolios_path, notice: "Portfolio item was successfully updated." }
       else
         format.html { render :edit }
       end
+    end
+  end
+  
+  def destroy
+    #@portfolio_item = Portfolio.find(params[:id])
+    
+    @portfolio_item.destroy
+    respond_to do |format|
+      format.html { redirect_to portfolios_path, notice: "Portfolio item deleted successfully." }
     end
   end
   
@@ -54,7 +66,9 @@ class PortfoliosController < ApplicationController
       params.require(:portfolio).permit(:title, :subtitle, :body, :main_image, :thumb_image)
     end
   
-  
+    def set_portfolio_item
+      @portfolio_item = Portfolio.find(params[:id])
+    end
   
   
   
