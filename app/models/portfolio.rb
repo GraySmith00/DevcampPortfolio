7c4c21f9b1a78c9775a0abed549a39db2c71b3dd
Jordan Hudgens <jordan@devcamp.com>
2017-05-24 20:56:27 +0000

    implemented placeholder concern in model  for managing image data

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,5 +1,5 @@
 class Portfolio < ApplicationRecord
-  
+  include Placeholder
   validates_presence_of :title, :body, :main_image, :thumb_image
   
   #scope :angular ~> { where(subtitle: 'Angular') }
@@ -17,11 +17,11 @@ class Portfolio < ApplicationRecord
   def set_defaults
     #self.main_image ||= "http://placehold.it/600x400"
     if self.main_image == nil
-      self.main_image = "http://placehold.it/600x400"
+      self.main_image = Placeholder.image_generator(height: '400', width: '600')
     end
     #self.thumb_image ||= "http://placehold.it/350x200"
     if self.thumb_image == nil
-      self.thumb_image = "http://placehold.it/350x200"
+      self.thumb_image = Placeholder.image_generator(height: '350', width: '200')
     end
   end
   
