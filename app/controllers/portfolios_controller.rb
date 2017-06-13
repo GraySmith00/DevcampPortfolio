dd07cc55d241fb0df72e00a34f4f76affff32461
Jordan Hudgens <jordan@devcamp.com>
2017-06-13 18:10:31 +0000

    styling and refactoring of portfolios controller code

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -16,7 +16,6 @@ class PortfoliosController < ApplicationController
   
   def show
     #@portfolio_item = Portfolio.find(params[:id])
-    
   end
   
   def new
@@ -66,7 +65,12 @@ class PortfoliosController < ApplicationController
   private
   
     def portfolio_params
-      params.require(:portfolio).permit(:title, :subtitle, :body, :main_image, :thumb_image, technologies_attributes: [:name])
+      params.require(:portfolio).permit(:title, 
+                                        :subtitle, 
+                                        :body, 
+                                        :main_image, 
+                                        :thumb_image, 
+                                        technologies_attributes: [:name])
     end
   
     def set_portfolio_item
