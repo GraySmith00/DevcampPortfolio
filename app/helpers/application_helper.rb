fcddc8bd89af99e318a907da4b8c77b8eb8bc2c1
Jordan Hudgens <jordan@devcamp.com>
2017-06-14 21:13:53 +0000

    implemented content tag and visiting source helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,18 @@ module ApplicationHelper
     end
   end
   
+  
+  
+  # <% if session[:source] %>
+  #   <p>Thanks for visiting me from <%= session[:source] %>!</p>
+  # <% end %>
+  def source_helper(layout_name)
+    if session[:source]
+      greeting = "Thanks for visiting me from #{session[:source]}! You are on my #{layout_name} layout!"
+      content_tag(:p, greeting, class: "source-greeting")
+    end
+  end
+  
+  
+  
 end
