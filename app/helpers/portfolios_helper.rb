93a07a54b24135b550f77ff669c608cd5b89224d
Jordan Hudgens <jordan@devcamp.com>
2017-07-12 00:24:42 +0000

    updated file upload forms and refactored placeholder functionality into view, also added dependent: :destroy for technologies in the portfolio model

--- app/helpers/portfolios_helper.rb
+++ app/helpers/portfolios_helper.rb
@@ -1,2 +1,21 @@
 module PortfoliosHelper
+  
+  
+  def image_generator(height:, width:)
+    "http://placehold.it/#{height}x#{width}"
+  end
+  
+  def portfolio_img(img, type)
+    if img.model.main_image? || img.model.thumb_image?
+      img
+    elsif type == 'thumb'
+      image_generator(height: '225', width: '225')
+    elsif type == 'main'
+      image_generator(height: '400', width: '600')
+    end
+  end
+  
+  
+  
+  
 end
--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,11 +1,10 @@
 class Portfolio < ApplicationRecord
-  has_many :technologies
+  has_many :technologies, dependent: :destroy
   # needed for accepting technology values in the portfolio form
   accepts_nested_attributes_for :technologies, 
           reject_if: lambda { |attrs| attrs['name'].blank? }
   
-  include Placeholder
-  validates_presence_of :title, :body, :main_image, :thumb_image
+  validates_presence_of :title, :body
   
   mount_uploader :thumb_image, PortfolioUploader
   mount_uploader :main_image, PortfolioUploader
@@ -20,21 +19,10 @@ class Portfolio < ApplicationRecord
     where(subtitle: "Ruby on Rails")
   end
   
-  after_initialize :set_defaults
-  
-  def set_defaults
-    #self.main_image ||= "http://placehold.it/600x400"
-    if self.main_image == nil
-      self.main_image = Placeholder.image_generator(height: '400', width: '600')
-    end
-    #self.thumb_image ||= "http://placehold.it/350x200"
-    if self.thumb_image == nil
-      self.thumb_image = Placeholder.image_generator(height: '350', width: '200')
-    end
-  end
-  
   def self.by_position
     order("position ASC")
   end
   
+ 
+  scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby on Rails') }
 end
